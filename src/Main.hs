2298a8644a93f6165e6999dc05761bfc0e221817
Joomy Korkut <joomy@cattheory.com>
2015-03-21 21:35:34 -0400

    Arrow keys allow combinations now

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Picture
 import System.Exit (exitSuccess)
 import Control.Concurrent (threadDelay)
-import Control.Monad (when, unless)
+import Control.Monad
+import qualified Data.List as L
 
 import Board
 import Drawing
@@ -31,6 +32,9 @@ windowWidth, windowHeight :: Int
 windowWidth  = 1000
 windowHeight = 800
 
+pressedAmong :: Window -> [Key] -> IO [Key]
+pressedAmong w = filterM (keyIsPressed w)
+
 main :: IO ()
 main = do
     glossState <- initState
@@ -38,28 +42,26 @@ main = do
     withWindow windowWidth windowHeight "Civ" $ \win -> do
           loop glossState gameState win
           exitSuccess
-  where loop glossState gameState window = do
-            threadDelay 20000
-            pollEvents
-            k <- keyIsPressed window Key'Escape
-            l <- keyIsPressed window Key'Left
-            r <- keyIsPressed window Key'Right
-            u <- keyIsPressed window Key'Up
-            d <- keyIsPressed window Key'Down
-            let newState = moveMap (l,r,u,d) gameState 10
-            renderFrame window glossState newState
-            unless k $ loop glossState newState window
+  where
+    loop glossState gameState window = do
+        threadDelay 20000
+        pollEvents
+        k <- keyIsPressed window Key'Escape
+        pressedArrow <- pressedAmong window [Key'Left, Key'Right, Key'Up, Key'Down]
+        let newState = moveMap pressedArrow gameState 10
+        renderFrame window glossState newState
+        unless k $ loop glossState newState window
 
 -- | Moves map to the opposite direction of the key.
-moveMap :: (Bool, Bool, Bool, Bool) -> GameState -> Float -> GameState
-moveMap b4 GameState{..} i =
-    GameState tileMapState (addOffset b4 mapPosition i)
+moveMap :: [Key] -> GameState -> Float -> GameState
+moveMap keys GameState{..} i =
+    GameState tileMapState (L.foldl' addOffset mapPosition keys)
   where
-    addOffset (True, _, _, _)              (x, y) i = (x + i, y)
-    addOffset (_, True, _, _)              (x, y) i = (x - i, y)
-    addOffset (_, _, True, _)              (x, y) i = (x, y - i)
-    addOffset (_, _, _, True)              (x, y) i = (x, y + i)
-    addOffset (False, False, False, False) (x, y) _ = (x, y)
+    addOffset (x, y) Key'Left  = (x + i, y)
+    addOffset (x, y) Key'Right = (x - i, y)
+    addOffset (x, y) Key'Up    = (x, y - i)
+    addOffset (x, y) Key'Down  = (x, y + i)
+    addOffset (x, y) _         = (x, y)
 
 renderFrame window glossState GameState{..} = do
      let (x, y) = mapPosition
