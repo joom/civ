e38cc242235198a6a81d18e9961c846ae98cc110
Joomy Korkut <joomy@cattheory.com>
2015-03-25 15:21:58 -0400

    Better event handling started, Elerea removed

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,65 @@
 {-# LANGUAGE PackageImports, RecordWildCards #-}
+-- PartialTypeSignatures should be used when it's available.
 module Main where
 
 import "GLFW-b" Graphics.UI.GLFW as GLFW
 import Graphics.Gloss.Rendering
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Picture
-import FRP.Elerea.Simple
 
 import System.Exit (exitSuccess)
 import Control.Concurrent (threadDelay)
+import Control.Applicative
+import Control.Concurrent.STM    (TQueue, atomically, newTQueueIO, tryReadTQueue, writeTQueue)
 import Control.Monad
 import Control.Monad.Fix (fix)
-import Control.Applicative
+import Control.Monad.RWS.Strict  (RWST, ask, asks, evalRWST, get, liftIO, modify, put)
+import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
+import Data.Maybe
 
 import Board
 import Drawing
 import GameState
+
+data Env = Env
+    { envEventsChan    :: TQueue Event
+    , envWindow        :: !GLFW.Window
+    }
+
+data State = State
+    { stateWindowWidth     :: !Int
+    , stateWindowHeight    :: !Int
+    , stateMouseDown       :: !Bool
+    , stateDragging        :: !Bool
+    , stateDragStartX      :: !Double
+    , stateDragStartY      :: !Double
+    , stateDragStartXAngle :: !Double
+    , stateDragStartYAngle :: !Double
+    , stateGameState       :: GameState
+    }
+
+type Game = RWST Env () State IO
+
+--------------------------------------------------------------------------------
+
+data Event =
+    EventError           !GLFW.Error !String
+  | EventWindowPos       !GLFW.Window !Int !Int
+  | EventWindowSize      !GLFW.Window !Int !Int
+  | EventWindowClose     !GLFW.Window
+  | EventWindowRefresh   !GLFW.Window
+  | EventWindowFocus     !GLFW.Window !GLFW.FocusState
+  | EventWindowIconify   !GLFW.Window !GLFW.IconifyState
+  | EventFramebufferSize !GLFW.Window !Int !Int
+  | EventMouseButton     !GLFW.Window !GLFW.MouseButton !GLFW.MouseButtonState !GLFW.ModifierKeys
+  | EventCursorPos       !GLFW.Window !Double !Double
+  | EventCursorEnter     !GLFW.Window !GLFW.CursorState
+  | EventScroll          !GLFW.Window !Double !Double
+  | EventKey             !GLFW.Window !GLFW.Key !Int !GLFW.KeyState !GLFW.ModifierKeys
+  | EventChar            !GLFW.Window !Char
+  deriving Show
+
+--------------------------------------------------------------------------------
 
 windowWidth, windowHeight :: Int
 windowWidth  = 1000
@@ -23,41 +67,76 @@ windowHeight = 800
 
 main :: IO ()
 main = do
-    (turnKey, turnKeySink)           <- external [] -- to move to the next unit
-    (zoomKey, zoomKeySink)           <- external [] -- for zoom
-    (arrowKey, arrowKeySink)         <- external [] -- for map movement
-    (directionKey, directionKeySink) <- external [] -- for hex direction
-    glossState <- initState
-    initialGS  <- initGameState
-    withWindow windowWidth windowHeight "Civ" $ \win -> do
-        network <- start $ do
-            gsSignal <-
-                transfer4 initialGS
-                          (\arrK dirK zoomK turnK gS@GameState{..} ->
-                              blink
-                              $ changeScale zoomK
-                              $ moveMap arrK 10
-                              $ turnAction turnK
-                              $ moveUnitWithKey unitPosition dirK gS)
-                          arrowKey directionKey zoomKey turnKey
-            return $ renderFrame win glossState <$> gsSignal
-        fix $ \loop -> do
-            readPressedInput win turnKeys turnKeySink
-            readPressedInput win zoomKeys zoomKeySink
-            readPressedInput win arrowKeys arrowKeySink
-            readPressedInput win directionKeys directionKeySink
-            join network
-            threadDelay 20000
-            esc <- keyIsPressed win Key'Escape
-            unless esc loop
-        exitSuccess
+    eventsChan <- newTQueueIO :: IO (TQueue Event)
 
-renderFrame window glossState gameState = do
-    displayPicture (windowWidth, windowHeight)
-                   seaColor glossState 1.0 (renderView gameState)
-    swapBuffers window
-  where
-    seaColor = makeColorI 10 105 148 1
+    withWindow windowWidth windowHeight "GLFW-b-demo" $ \win -> do
+        GLFW.setErrorCallback               $ Just $ errorCallback           eventsChan
+        GLFW.setWindowPosCallback       win $ Just $ windowPosCallback       eventsChan
+        GLFW.setWindowSizeCallback      win $ Just $ windowSizeCallback      eventsChan
+        GLFW.setWindowCloseCallback     win $ Just $ windowCloseCallback     eventsChan
+        GLFW.setWindowRefreshCallback   win $ Just $ windowRefreshCallback   eventsChan
+        GLFW.setWindowFocusCallback     win $ Just $ windowFocusCallback     eventsChan
+        GLFW.setWindowIconifyCallback   win $ Just $ windowIconifyCallback   eventsChan
+        GLFW.setFramebufferSizeCallback win $ Just $ framebufferSizeCallback eventsChan
+        GLFW.setMouseButtonCallback     win $ Just $ mouseButtonCallback     eventsChan
+        GLFW.setCursorPosCallback       win $ Just $ cursorPosCallback       eventsChan
+        GLFW.setCursorEnterCallback     win $ Just $ cursorEnterCallback     eventsChan
+        GLFW.setScrollCallback          win $ Just $ scrollCallback          eventsChan
+        GLFW.setKeyCallback             win $ Just $ keyCallback             eventsChan
+        GLFW.setCharCallback            win $ Just $ charCallback            eventsChan
+
+        GLFW.swapInterval 1
+
+        (fbWidth, fbHeight) <- GLFW.getFramebufferSize win
+        glossState <- initState     -- we don't have access to its type
+        gameState  <- initGameState
+
+        let env = Env
+              { envEventsChan    = eventsChan
+              , envWindow        = win
+              }
+            state = State
+              { stateWindowWidth     = fbWidth
+              , stateWindowHeight    = fbHeight
+              , stateMouseDown       = False
+              , stateDragging        = False
+              , stateDragStartX      = 0
+              , stateDragStartY      = 0
+              , stateDragStartXAngle = 0
+              , stateDragStartYAngle = 0
+              , stateGameState       = gameState
+              }
+
+        runGame glossState env state
+
+    -- (turnKey, turnKeySink)           <- external [] -- to move to the next unit
+    -- (zoomKey, zoomKeySink)           <- external [] -- for zoom
+    -- (arrowKey, arrowKeySink)         <- external [] -- for map movement
+    -- (directionKey, directionKeySink) <- external [] -- for hex direction
+    -- glossState <- initState
+    -- initialGS  <- initGameState
+    -- withWindow windowWidth windowHeight "Civ" $ \win -> do
+    --     network <- start $ do
+    --         gsSignal <-
+    --             transfer4 initialGS
+    --                       (\arrK dirK zoomK turnK gS@GameState{..} ->
+    --                           blink
+    --                           $ changeScale zoomK
+    --                           $ moveMap arrK 10
+    --                           $ turnAction turnK
+    --                           $ moveUnitWithKey unitPosition dirK gS)
+    --                       arrowKey directionKey zoomKey turnKey
+    --         return $ renderFrame win glossState <$> gsSignal
+    --     fix $ \loop -> do
+    --         readPressedInput win turnKeys turnKeySink
+    --         readPressedInput win zoomKeys zoomKeySink
+    --         readPressedInput win arrowKeys arrowKeySink
+    --         readPressedInput win directionKeys directionKeySink
+    --         join network
+    --         threadDelay 20000
+    --         esc <- keyIsPressed win Key'Escape
+    --         unless esc loop
+    --     exitSuccess
 
 -- | Checks which of the given keys are pressed.
 pressedAmong :: Window -> [Key] -> IO [Key]
@@ -93,6 +172,228 @@ withWindow width height title f = do
     simpleErrorCallback e s =
         putStrLn $ unwords [show e, show s]
 
+--------------------------------------------------------------------------------
+
+-- Each callback does just one thing: write an appropriate Event to the events
+-- TQueue.
+
+errorCallback           :: TQueue Event -> GLFW.Error -> String                                                            -> IO ()
+windowPosCallback       :: TQueue Event -> GLFW.Window -> Int -> Int                                                       -> IO ()
+windowSizeCallback      :: TQueue Event -> GLFW.Window -> Int -> Int                                                       -> IO ()
+windowCloseCallback     :: TQueue Event -> GLFW.Window                                                                     -> IO ()
+windowRefreshCallback   :: TQueue Event -> GLFW.Window                                                                     -> IO ()
+windowFocusCallback     :: TQueue Event -> GLFW.Window -> GLFW.FocusState                                                  -> IO ()
+windowIconifyCallback   :: TQueue Event -> GLFW.Window -> GLFW.IconifyState                                                -> IO ()
+framebufferSizeCallback :: TQueue Event -> GLFW.Window -> Int -> Int                                                       -> IO ()
+mouseButtonCallback     :: TQueue Event -> GLFW.Window -> GLFW.MouseButton   -> GLFW.MouseButtonState -> GLFW.ModifierKeys -> IO ()
+cursorPosCallback       :: TQueue Event -> GLFW.Window -> Double -> Double                                                 -> IO ()
+cursorEnterCallback     :: TQueue Event -> GLFW.Window -> GLFW.CursorState                                                 -> IO ()
+scrollCallback          :: TQueue Event -> GLFW.Window -> Double -> Double                                                 -> IO ()
+keyCallback             :: TQueue Event -> GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys            -> IO ()
+charCallback            :: TQueue Event -> GLFW.Window -> Char                                                             -> IO ()
+
+errorCallback           tc e s            = atomically $ writeTQueue tc $ EventError           e s
+windowPosCallback       tc win x y        = atomically $ writeTQueue tc $ EventWindowPos       win x y
+windowSizeCallback      tc win w h        = atomically $ writeTQueue tc $ EventWindowSize      win w h
+windowCloseCallback     tc win            = atomically $ writeTQueue tc $ EventWindowClose     win
+windowRefreshCallback   tc win            = atomically $ writeTQueue tc $ EventWindowRefresh   win
+windowFocusCallback     tc win fa         = atomically $ writeTQueue tc $ EventWindowFocus     win fa
+windowIconifyCallback   tc win ia         = atomically $ writeTQueue tc $ EventWindowIconify   win ia
+framebufferSizeCallback tc win w h        = atomically $ writeTQueue tc $ EventFramebufferSize win w h
+mouseButtonCallback     tc win mb mba mk  = atomically $ writeTQueue tc $ EventMouseButton     win mb mba mk
+cursorPosCallback       tc win x y        = atomically $ writeTQueue tc $ EventCursorPos       win x y
+cursorEnterCallback     tc win ca         = atomically $ writeTQueue tc $ EventCursorEnter     win ca
+scrollCallback          tc win x y        = atomically $ writeTQueue tc $ EventScroll          win x y
+keyCallback             tc win k sc ka mk = atomically $ writeTQueue tc $ EventKey             win k sc ka mk
+charCallback            tc win c          = atomically $ writeTQueue tc $ EventChar            win c
+
+--------------------------------------------------------------------------------
+
+-- runGame :: _ -> Env -> State -> IO ()
+runGame glossState env state = do
+    -- printInstructions
+    void $ evalRWST (adjustWindow >> run glossState) env state
+
+-- run :: _ -> Game ()
+run glossState = do
+    win <- asks envWindow
+
+    draw glossState
+    liftIO $ do
+        GLFW.swapBuffers win
+        GLFW.pollEvents
+    processEvents
+
+    state <- get
+    if stateDragging state
+      then do
+          let sodx  = stateDragStartX      state
+              sody  = stateDragStartY      state
+              sodxa = stateDragStartXAngle state
+              sodya = stateDragStartYAngle state
+          (x, y) <- liftIO $ GLFW.getCursorPos win
+          let myrot = (x - sodx) / 2
+              mxrot = (y - sody) / 2
+          put $ state
+            -- { stateXAngle = sodxa + mxrot
+            -- , stateYAngle = sodya + myrot
+            -- }
+      else do
+          (kxrot, kyrot) <- liftIO $ getCursorKeyDirections win
+          (jxrot, jyrot) <- liftIO $ getJoystickDirections GLFW.Joystick'1
+          put $ state
+            -- { stateXAngle = stateXAngle state + (2 * kxrot) + (2 * jxrot)
+            -- , stateYAngle = stateYAngle state + (2 * kyrot) + (2 * jyrot)
+            -- }
+
+    mt <- liftIO GLFW.getTime
+    modify $ \s -> s
+      -- { stateGearZAngle = maybe 0 (realToFrac . (100*)) mt
+      -- }
+
+    q <- liftIO $ GLFW.windowShouldClose win
+    unless q (run glossState)
+
+processEvents :: Game ()
+processEvents = do
+    tc <- asks envEventsChan
+    me <- liftIO $ atomically $ tryReadTQueue tc
+    case me of
+      Just e -> do
+          processEvent e
+          processEvents
+      Nothing -> return ()
+
+processEvent :: Event -> Game ()
+processEvent ev =
+    case ev of
+      (EventError e s) -> do
+          printEvent "error" [show e, show s]
+          win <- asks envWindow
+          liftIO $ GLFW.setWindowShouldClose win True
+
+      (EventWindowPos _ x y) ->
+          printEvent "window pos" [show x, show y]
+
+      (EventWindowSize _ width height) ->
+          printEvent "window size" [show width, show height]
+
+      (EventWindowClose _) ->
+          printEvent "window close" []
+
+      (EventWindowRefresh _) ->
+          printEvent "window refresh" []
+
+      (EventWindowFocus _ fs) ->
+          printEvent "window focus" [show fs]
+
+      (EventWindowIconify _ is) ->
+          printEvent "window iconify" [show is]
+
+      (EventFramebufferSize _ width height) -> do
+          printEvent "framebuffer size" [show width, show height]
+          modify $ \s -> s
+            { stateWindowWidth  = width
+            , stateWindowHeight = height
+            }
+          adjustWindow
+
+      (EventMouseButton _ mb mbs mk) -> do
+          printEvent "mouse button" [show mb, show mbs, showModifierKeys mk]
+          when (mb == GLFW.MouseButton'1) $ do
+              let pressed = mbs == GLFW.MouseButtonState'Pressed
+              modify $ \s -> s
+                { stateMouseDown = pressed
+                }
+              unless pressed $
+                modify $ \s -> s
+                  { stateDragging = False
+                  }
+
+      (EventCursorPos _ x y) -> do
+          let x' = round x :: Int
+              y' = round y :: Int
+          printEvent "cursor pos" [show x', show y']
+          state <- get
+          when (stateMouseDown state && not (stateDragging state)) $
+            put $ state
+              { stateDragging        = True
+              , stateDragStartX      = x
+              , stateDragStartY      = y
+              -- , stateDragStartXAngle = stateXAngle state
+              -- , stateDragStartYAngle = stateYAngle state
+              }
+
+      (EventCursorEnter _ cs) ->
+          printEvent "cursor enter" [show cs]
+
+      (EventScroll _ x y) -> do
+          let x' = round x :: Int
+              y' = round y :: Int
+          printEvent "scroll" [show x', show y']
+          env <- ask
+          modify $ \s -> s -- TODO
+          adjustWindow
+
+      (EventKey win k scancode ks mk) -> do
+          printEvent "key" [show k, show scancode, show ks, showModifierKeys mk]
+          when (ks == GLFW.KeyState'Pressed) $ do
+              -- Q, Esc: exit
+              when (k == GLFW.Key'Q || k == GLFW.Key'Escape) $
+                liftIO $ GLFW.setWindowShouldClose win True
+              -- TODO
+
+              -- -- ?: print instructions
+              -- when (k == GLFW.Key'Slash && GLFW.modifierKeysShift mk) $
+              --   liftIO printInstructions
+              -- -- i: print GLFW information
+              -- when (k == GLFW.Key'I) $
+              --   liftIO $ printInformation win
+
+      (EventChar _ c) ->
+          printEvent "char" [show c]
+
+adjustWindow :: Game ()
+adjustWindow = do
+    state <- get
+    let width  = stateWindowWidth  state
+        height = stateWindowHeight state
+
+    return () -- TODO
+
+-- draw :: _ -> Game ()
+draw glossState = do
+    env   <- ask
+    state <- get
+    liftIO $ displayPicture
+                 (windowWidth, windowHeight)
+                  seaColor glossState 1.0 (renderView (stateGameState state))
+    liftIO $ return ()
+  where
+    seaColor = makeColorI 10 105 148 1
+
+--------------------------------------------------------------------------------
+
+getCursorKeyDirections :: GLFW.Window -> IO (Double, Double)
+getCursorKeyDirections win = do
+    x0 <- keyIsPressed win GLFW.Key'Up
+    x1 <- keyIsPressed win GLFW.Key'Down
+    y0 <- keyIsPressed win GLFW.Key'Left
+    y1 <- keyIsPressed win GLFW.Key'Right
+    let x0n = if x0 then (-1) else 0
+        x1n = if x1 then   1  else 0
+        y0n = if y0 then (-1) else 0
+        y1n = if y1 then   1  else 0
+    return (x0n + x1n, y0n + y1n)
+
+getJoystickDirections :: GLFW.Joystick -> IO (Double, Double)
+getJoystickDirections js = do
+    maxes <- GLFW.getJoystickAxes js
+    return $ case maxes of
+      (Just (x:y:_)) -> (-y, x)
+      _              -> ( 0, 0)
+
+
 keyIsPressed :: Window -> Key -> IO Bool
 keyIsPressed win key = isPress `fmap` GLFW.getKey win key
 
@@ -100,3 +401,21 @@ isPress :: KeyState -> Bool
 isPress KeyState'Pressed   = True
 isPress KeyState'Repeating = True
 isPress _                  = False
+
+--------------------------------------------------------------------------------
+
+printEvent :: String -> [String] -> Game ()
+printEvent cbname fields =
+    liftIO $ putStrLn $ cbname ++ ": " ++ unwords fields
+
+showModifierKeys :: GLFW.ModifierKeys -> String
+showModifierKeys mk =
+    "[mod keys: " ++ keys ++ "]"
+  where
+    keys = if null xs then "none" else unwords xs
+    xs = catMaybes ys
+    ys = [ if GLFW.modifierKeysShift   mk then Just "shift"   else Nothing
+         , if GLFW.modifierKeysControl mk then Just "control" else Nothing
+         , if GLFW.modifierKeysAlt     mk then Just "alt"     else Nothing
+         , if GLFW.modifierKeysSuper   mk then Just "super"   else Nothing
+         ]
